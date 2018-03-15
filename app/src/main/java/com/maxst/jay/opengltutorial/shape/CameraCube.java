177c1700d66988ad93fad7d9b27773cdf1084115
Jeonguk <jeonguk04@gmail.com>
2018-03-15 15:30:55 +0900

    no message

--- app/src/main/java/com/maxst/jay/opengltutorial/shape/CameraCube.java
+++ app/src/main/java/com/maxst/jay/opengltutorial/shape/CameraCube.java
@@ -24,30 +24,28 @@ public class CameraCube extends BaseShape implements SurfaceTexture.OnFrameAvail
     private boolean isRequiredUpdateTexture = false;
 
     private final String vertexShader =
-            "attribute vec4 a_position;" +
-                    "attribute vec4 a_color;" +
-                    "attribute vec3 a_normal;" +
-                    "uniform mat4 u_VPMatrix;" +
-                    "uniform vec3 u_LightPos;" +
-                    "varying vec3 v_texCoords;" +
-                    "attribute vec3 a_texCoords;" +
-                    "void main()" +
-                    "{" +
-                    "v_texCoords = a_texCoords;" +
-                    "gl_Position = u_VPMatrix * a_position;" +
-                    "}";
+            "attribute vec4 a_position;\n" +
+                    "attribute vec2 a_texCoord;\n" +
+                    "varying vec2 v_texCoord;\n" +
+                    "uniform mat4 u_mvpMatrix;\n" +
+                    "void main()							\n" +
+                    "{										\n" +
+                    "	gl_Position = u_mvpMatrix * a_position;\n" +
+                    "	v_texCoord = a_texCoord; 			\n" +
+                    "}										\n";
 
     private final String fragmentShader =
-            "precision mediump float;" +
-                    "uniform samplerCube u_texId;" +
-                    "varying vec3 v_texCoords;" +
-                    "void main()" +
-                    "{" +
-                    "gl_FragColor = textureCube(u_texId, v_texCoords);" +
-                    "}";
+            "#extension GL_OES_EGL_image_external : require\n" +
+            "precision mediump float;\n" +
+                    "varying vec2 v_texCoord;\n" +
+                    "uniform samplerExternalOES u_texture;\n" +
+                    "void main(void)\n" +
+                    "{\n" +
+                    "	gl_FragColor = texture2D(u_texture, v_texCoord);\n" +
+                    "}\n";
 
 
-    private float[] VERTEX_BUF = {
+    private final float VERTEX_BUF[] = {
             1, 1, 1, -1, 1, 1, -1, -1, 1, 1, -1, 1,
             1, 1, 1, 1, -1, 1, 1, -1, -1, 1, 1, -1,
             1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, -1,
@@ -56,22 +54,22 @@ public class CameraCube extends BaseShape implements SurfaceTexture.OnFrameAvail
             1, -1, 1, -1, -1, 1, -1, -1, -1, 1, -1, -1,
     };
 
-    private short[] INDEX_BUF = {
+    private final float TEXTURE_BUF[] = {
+            0, 1, 1, 1, 1, 0, 0, 0,
+            0, 1, 1, 1, 1, 0, 0, 0,
+            0, 1, 1, 1, 1, 0, 0, 0,
+            0, 1, 1, 1, 1, 0, 0, 0,
+            0, 1, 1, 1, 1, 0, 0, 0,
+            0, 1, 1, 1, 1, 0, 0, 0,
+    };
+
+    private final short INDEX_BUF[] = {
             0, 1, 2, 0, 2, 3,
             4, 5, 6, 4, 6, 7,
             8, 9, 10, 8, 10, 11,
             12, 13, 14, 12, 14, 15,
             16, 17, 18, 16, 18, 19,
             20, 21, 22, 20, 22, 23,
-    };
-
-    private float[] TEXTURE_BUF = {
-            1, 1, 1, -1, 1, 1, -1, -1, 1, 1, -1, 1, //0-1-2-3 front
-            1, 1, 1, 1, -1, 1, 1, -1, -1, 1, 1, -1,//0-3-4-5 right
-            1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, -1,//4-7-6-5 back
-            -1, 1, 1, -1, 1, -1, -1, -1, -1, -1, -1, 1,//1-6-7-2 left
-            1, 1, 1, 1, 1, -1, -1, 1, -1, -1, 1, 1, //top
-            1, -1, 1, -1, -1, 1, -1, -1, -1, 1, -1, -1,//bottom
     };
 
     public CameraCube(GLSurfaceView surfaceView) {
@@ -89,18 +87,19 @@ public class CameraCube extends BaseShape implements SurfaceTexture.OnFrameAvail
 
         GLES20.glEnable(GLES20.GL_DEPTH_TEST);
         GLES20.glDepthFunc(GLES20.GL_LEQUAL);
-        GLES20.glFrontFace(GLES20.GL_CCW);
+//        GLES20.glFrontFace(GLES20.GL_CCW);
         GLES20.glEnable(GLES20.GL_CULL_FACE);
         GLES20.glCullFace(GLES20.GL_BACK);
         GLES20.glEnable(GLES20.GL_BLEND);
-        GLES20.glBlendFunc(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA);
+//        GLES20.glBlendFunc(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA);
+
 
         programID = ShaderUtil.createProgram(vertexShader, fragmentShader);
 
         positionHandle = GLES20.glGetAttribLocation(programID, "a_position");
-        mvpMatrixHandle = GLES20.glGetUniformLocation(programID, "u_VPMatrix");
-        textureHandle = GLES20.glGetUniformLocation(programID, "u_texId");
-        textureCoordHandle = GLES20.glGetAttribLocation(programID, "a_texCoords");
+        mvpMatrixHandle = GLES20.glGetUniformLocation(programID, "u_mvpMatrix");
+        textureHandle = GLES20.glGetUniformLocation(programID, "u_texture");
+        textureCoordHandle = GLES20.glGetAttribLocation(programID, "a_texCoord");
 
 
         createCubeTexture();
@@ -125,11 +124,11 @@ public class CameraCube extends BaseShape implements SurfaceTexture.OnFrameAvail
         GLES20.glEnableVertexAttribArray(positionHandle);
 
         textureBuffer.position(0);
-        GLES20.glVertexAttribPointer(textureCoordHandle, 3, GLES20.GL_FLOAT, false, 0, textureBuffer);
+        GLES20.glVertexAttribPointer(textureCoordHandle, 2, GLES20.GL_FLOAT, false, 0, textureBuffer);
         GLES20.glEnableVertexAttribArray(textureCoordHandle);
 
         GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
-        GLES20.glBindTexture(GLES20.GL_TEXTURE_CUBE_MAP, textureID[0]);
+        GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, textureID[0]);
         GLES20.glUniform1i(textureHandle, 0);
 
         float[] tmpMatrix = new float[16];
@@ -151,11 +150,11 @@ public class CameraCube extends BaseShape implements SurfaceTexture.OnFrameAvail
     public void createCubeTexture() {
         textureID = new int[1];
         GLES20.glGenTextures(1, textureID, 0);
-        GLES20.glBindTexture(GLES20.GL_TEXTURE_CUBE_MAP, textureID[0]);
-        GLES20.glTexParameteri(GLES20.GL_TEXTURE_CUBE_MAP, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
-        GLES20.glTexParameteri(GLES20.GL_TEXTURE_CUBE_MAP, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
-        GLES20.glTexParameteri(GLES20.GL_TEXTURE_CUBE_MAP, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST);
-        GLES20.glTexParameteri(GLES20.GL_TEXTURE_CUBE_MAP, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_NEAREST);
+        GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, textureID[0]);
+        GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST);
+        GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_NEAREST);
 
         surfaceTexture = new SurfaceTexture(textureID[0]);
         surfaceTexture.setOnFrameAvailableListener(this);
